50a1ecb8e18e7766e4171fcdd5afc5bd4a2f882c
gurvindrasingh <gurvindrasingh@email.com>
2025-04-25 18:58:10 -0400

    UserRegistered event to update profile in user service

--- services/auth-service/src/services/auth.service.ts
+++ services/auth-service/src/services/auth.service.ts
@@ -1,8 +1,11 @@
 import bcrypt from "bcryptjs";
 import prisma from "./prisma";
 import { signJwt } from "../utils/jwt";
+import { EventBridgeClient, PutEventsCommand } from '@aws-sdk/client-eventbridge';
 
-export const signupUser = async ({ name, email, password }: any) => {
+const eventBridge = new EventBridgeClient({ region: 'us-east-1' });
+
+export const signupUser = async ({ name, email, password, phone, bio }: any) => {
   const existingUser = await prisma.user.findUnique({ where: { email } });
   if (existingUser) throw new Error("User already exists");
 
@@ -10,6 +13,20 @@ export const signupUser = async ({ name, email, password }: any) => {
   const user = await prisma.user.create({
     data: { name, email, password: hashedPassword },
   });
+
+  //Emit UserRegistered event to EventBridge
+  const command = new PutEventsCommand({
+    Entries: [
+      {
+        Source: 'auth-service',
+        DetailType: 'UserRegistered',
+        Detail: JSON.stringify({ id: user.id, name, phone, bio }),
+        EventBusName: 'default',
+      },
+    ],
+  });
+
+  await eventBridge.send(command);
 
   const token = signJwt({ id: user.id });
 
